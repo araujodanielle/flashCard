5199807a7cf1a6c2594134916f9a648f9ddaae86
Danielle de Souza Araújo <danicreamas@gmail.com>
2024-09-12 15:49:14 -0300

    versão inicial ( antes da Alura concluir o curso)

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criacartao(
+    'Programação',
+    'O que é Python?',
+    'O Paython é uma linguagem de programação.'
+)
+
+criacartao(
+    'Geografia',
+    'Qual a capital da França',
+    ' A capital da França é Paris'
+)
+
+
+criacartao(
+    'Programação',
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criacartao(
+    'Língua inglesa',
+    'Como se diz "oi" em inglês?',
+    ' Hi'
+)
+
+criacartao(
+    'Matemática',
+    'Quanto é 2x25?',
+    'é 50'
+)
+
+criacartao(
+    'História',
+    'Qual a data da independência do Brasil?',
+    'A independência do Brasil é celebrada em 7 de setembro'
+)
